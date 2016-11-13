fa1634ebf0c0213d16ba11bc5bb3a2ae5e7473c0
Sebastian Nepote <sebastian.nepote@helpling.com>
2016-11-13 14:13:24 +0100

    WIP TimeToEvent

--- spec/models/services/time_to_event_spec.rb
+++ spec/models/services/time_to_event_spec.rb
@@ -1,21 +1,28 @@
 require 'rails_helper'
 
 class TimeToEvent
-  attr_accessor :name
+
+  def initialize( event=DateTime.new )
+    @event = event
+    @now = DateTime.now
+  end
+
+  def days
+    (@event.mjd - @now.mjd).to_i
+  end
 end
 
 RSpec.describe TimeToEvent, :type => :model do
-  subject { described_class.new }
+  subject { described_class.new DateTime.parse('2016-01-06 00:00:00') } # 5 days
   before do
-    Timecop.freeze('2016-01-01 00:00:00')
+    Timecop.freeze(DateTime.parse('2016-01-01 00:00:00'))
   end
 
   after do
     Timecop.return
-  end  
+  end
 
   it "is valid with valid attributes" do
-    subject.name = 'Anything'
-    expect(subject.name).to eq 'Anything'
+    expect(subject.days).to eq 5
   end
 end
