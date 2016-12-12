38a38ddaee657c3712e902d5bb3f9c1fcc78f049
Sebastian Nepote <sebastian.nepote@helpling.com>
2016-12-12 20:02:38 +0100

    Improves hounds complains

--- spec/models/services/time_percentage_spec.rb
+++ spec/models/services/time_percentage_spec.rb
@@ -13,10 +13,10 @@ RSpec.describe Services::TimePercentage, :type => :model do
 
   context '8 months, 3 weeks, 4 days' do
     with_weeks = TRUE
-    let(:remaining) {Services::TimeToEvent.new DateTime.parse('2017-09-02 00:00:00'), TRUE}
-    it { expect(subject.months).to eq 67}
-    it { expect(subject.weeks).to eq 75}
-    it { expect(subject.days).to eq 57}
+    let(:remaining) { Services::TimeToEvent.new DateTime.parse('2017-09-02 00:00:00'), with_weeks }
+    it { expect(subject.months).to eq 67 }
+    it { expect(subject.weeks).to eq 75 }
+    it { expect(subject.days).to eq 57 }
     it { expect(subject.to_s).to eq "8 months 3 weeks 4 days"}
   end
 end
--- spec/models/time_to_wedding_spec.rb
+++ spec/models/time_to_wedding_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe TimeToWedding, :type => :model do
+RSpec.describe TimeToWedding, type: :model do
   before do
     Timecop.freeze(DateTime.parse('2016-12-11 00:00:00'))
   end
@@ -14,12 +14,12 @@ RSpec.describe TimeToWedding, :type => :model do
   describe '#get' do
     context '2017-09-02 00:00:00' do
       collection = {
-            months: {percent: 67, value: 8, name: 'months'},
-            weeks: {percent: 75, value: 3, name: 'weeks'},
-            days: {percent: 57, value: 4, name: 'days'},
-          }
+        months: { percent: 67, value: 8, name: 'months' },
+        weeks: { percent: 75, value: 3, name: 'weeks' },
+        days: { percent: 57, value: 4, name: 'days' },
+      }
       let(:event_datetime) { DateTime.parse '2017-09-02 00:00:00' }
-      it { expect(subject.get).to eql collection}
+      it { expect(subject.get).to eql collection }
     end
   end
 end
