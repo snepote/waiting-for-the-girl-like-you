2c7ee9685e24a7157c7c1329bb729a2dd5f3779b
Sebastian Nepote <sebastian.nepote@helpling.com>
2016-12-26 17:27:46 +0100

    i18n title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = 'Rails material design with Materialize'
+    base_title = t 'website.title'
     if page_title.empty?
       base_title
     else
