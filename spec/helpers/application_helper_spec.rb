bdf60e6878e558236df30c0695c428188b110cd2
Sebastian Nepote <sebastian.nepote@helpling.com>
2016-12-26 17:40:19 +0100

    Fix tests for title

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'.freeze
 
 RSpec.describe ApplicationHelper do
   subject { Object.new.extend(ApplicationHelper) }
-  let(:base_title) { 'Rails material design with Materialize' }
+  let(:base_title) { 'Waiting for a girl like you' }
   it 'returns a sample title with the website base title' do
     title = "sample title | #{base_title}"
     expect(subject.full_title('sample title')).to eq title
