0f1607006de0eb664d30a89f16819d97db5a5f3b
Sebastian Nepote <sebastian.nepote@helpling.com>
2017-04-06 11:47:33 +0200

    Removes useless pages and tests

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-feature 'Homepage' do
-  pending 'temporarily disabled: not working on travis-ci'
+feature 'Homepage feature' do
+  pending 'is temporarily disabled: not working on travis-ci'
   # scenario 'As a user I want to see "Hause" in the homepage', type: :feature, js: true do
   #   visit '/'
   #   expect(page).to have_content('Hause')
